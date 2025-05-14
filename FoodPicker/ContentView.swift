2275c75d1e0ae016a7859ef0cfc9e47604baaa73
Sharawadgi <60963002+qianzhuiwuneng@users.noreply.github.com>
2025-05-14 19:52:31 +0800

    Initial Commit

---
+++ FoodPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPicker
+//
+//  Created by Sharawadgi on 2025/5/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
